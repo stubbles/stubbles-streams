6c2856123448fff02a1b54b500b6cf5f8ec4e8c3
Frank Kleine <mikey179@users.noreply.github.com>
2025-11-30 13:12:58 +0100

    fix implicitly nullable parameters

--- src/main/php/StreamException.php
+++ src/main/php/StreamException.php
@@ -15,7 +15,7 @@ use Throwable;
  */
 class StreamException extends Exception
 {
-    public function __construct(string $message, Throwable $previous = null)
+    public function __construct(string $message, ?Throwable $previous = null)
     {
         parent::__construct($message, 0, $previous);
     }
